b94c89acf9f2241fb8f444c1015f153bdc11e4f6
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-04-22 13:47:36 +0200

    first commit

---
+++ components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <header
+      :class="isOpen ? 'bg-gray-900' : 'bg-black'"
+      class="transition duration-1000 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3"
+    >
+      <div class="flex items-center justify-between px-4 py-3 sm:p-0 lg:ml-4">
+        <p class="text-white text-l font-bold xl:text-2xl">CryptoInfo.</p>
+
+        <div class="sm:hidden">
+          <img
+            src="@/assets/images/hamburger.png"
+            alt="logo"
+            class="block h-6 w-6 "
+            @click="isOpen = !isOpen"
+          />
+        </div>
+      </div>
+      <div
+        :class="isOpen ? 'block' : 'hidden'"
+        class="text-white px-2 pt-2 pb-4 sm:flex sm:p-0 lg:mr-4"
+      >
+        <a href="" class="block px-2 py-1">Link 1 </a>
+        <a href="" class="block mt-1 px-2 py-1 sm:mt-0 sm:ml-2">Link 1 </a>
+        <a href="" class="block mt-1 px-2 py-1 sm:mt-0 sm:ml-2">Link 1 </a>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="min-h-screen bg-black">
+    <NavBar />
+    <div class="xl:flex xl:justify-center">
+      <section
+        class="flex flex-col items-center justify-around min-h-screen -mt-8 sm:mt-4 md:flex-row md:items-center md:justify-around md:mt-56 md:min-h-full lg:mt-24
+         xl:justify-between xl:mt-48 xl:w-11/12"
+      >
+        <div class="text-white flex flex-col ">
+          <div class="border-b-2 border-red-700 w-12"></div>
+          <div>
+            <h1 class="text-white text-2xl mt-4 md:text-4xl lg:text-6xl">
+              We´re a digital
+            </h1>
+            <span class="text-xl md:2xl lg:text-4xl">info agency.</span>
+          </div>
+
+          <a
+            href=""
+            class=" border-2 border-red-700 text-center mt-4 p-4 hover:bg-red-700 transition-colors duration-500 ease-in-out md:text-xl lg:w-4/12"
+            >Get Started</a
+          >
+        </div>
+
+        <div class="hidden md:flex xl:w-2/5">
+          <img src="@/assets/images/watch.png" alt="mobile phone" />
+        </div>
+
+        <div class="md:hidden">
+          <img
+            src="@/assets/images/arrow-down.png"
+            alt="arrow"
+            @click="goTo"
+            class="cursor-pointer "
+          />
+        </div>
+      </section>
+    </div>
+    <section
+      class="flex flex-col items-center pb-12 text-white md:mt-56 md:pb-40 lg:flex-row lg:justify-around lg:mt-52 "
+      ref="info"
+    >
+      <div class="bg-red-600 w-3/4 px-8 py-16 mt-8 md:w-6/12 lg:w-3/12 ">
+        <h2
+          class="inline-block text-xl pt-4 pb-2 border-b-2 align-text-top md:text-4xl"
+        >
+          Vision
+        </h2>
+        <p class="mt-4 md:text-xl">
+          Our revolutionary feature set is unmatched, but our seamless
+          performance and easy use is often considered an amazing achievement.
+        </p>
+      </div>
+      <div class="bg-gray-800 w-3/4 px-8 py-16 mt-8 md:w-6/12 lg:w-3/12">
+        <h2
+          class="inline-block text-xl pt-4 pb-2 border-b-2 align-text-top md:text-4xl"
+        >
+          Vision
+        </h2>
+        <p class="mt-4 md:text-xl">
+          Our revolutionary feature set is unmatched, but our seamless
+          performance and easy use is often considered an amazing achievement.
+        </p>
+      </div>
+      <div class="bg-gray-900 w-3/4 px-8 py-16 mt-8 md:w-6/12 lg:w-3/12">
+        <h2
+          class="inline-block text-xl pt-4 pb-2 border-b-2 align-text-top md:text-4xl"
+        >
+          Vision
+        </h2>
+        <p class="mt-4 md:text-xl">
+          Our revolutionary feature set is unmatched, but our seamless
+          performance and easy use is often considered an amazing achievement.
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goTo() {
+      this.$refs.info.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
