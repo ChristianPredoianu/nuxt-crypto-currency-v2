454ff4fcf4df9b515e438a3d460e77352fc08cc3
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-04-29 21:08:33 +0200

    added search function

--- pages/exchange.vue
+++ pages/exchange.vue
@@ -19,32 +19,52 @@
       </div>
     </section>
 
-    <section class="py-12">
-      <div class="flex flex-wrap justify-between px-20 gap-2 lg:flex-no-wrap">
+    <section class="py-40">
+      <div
+        class="flex flex-wrap justify-between px-8 gap-8 text-center lg:flex-no-wrap"
+      >
         <TopCoinExchange
-          v-for="coin in coinData.slice(0, 4)"
+          v-for="coin in currencyData.slice(0, 4)"
           :key="coin.id"
           :coin="coin"
           class="w-5/12 lg:w-2/12"
         />
       </div>
+      <div class="flex justify-end items-center mt-12 mr-12 py-4 ">
+        <p class="text-xl invisible">ghost element</p>
+        <input
+          type="search"
+          class="w-4/12 px-2 mr-4 "
+          placeholder="Search"
+          aria-label="Search currency"
+          v-if="isSearchOpen"
+          v-model="searchTerm"
+        />
+        <font-awesome-icon
+          class="text-xl text-white"
+          :icon="['fas', 'search']"
+          @click="isSearchOpen = !isSearchOpen"
+        />
+      </div>
 
-      <div class="text-gray-400 pt-12 pb-2 lg:px-20">
+      <div class="text-gray-400 pb-2 mt-12 lg:px-20">
         <ul class="flex px-8 text-center">
-          <li class="w-4/12 lg:w-2/12">Currency</li>
           <li class="w-4/12 lg:w-2/12">
+            Currency
+          </li>
+          <li class="w-4/12 flex flex-col justify-between lg:w-2/12">
             Price
-            <div class="flex flex-col items-center">
+            <div class="mt-8">
               <font-awesome-icon
-                class="text-3xl"
+                class="text-3xl "
                 :icon="['fas', 'sort-up']"
                 @click="sort('price', 'asc')"
               />
             </div>
           </li>
-          <li class="w-4/12 lg:w-2/12">
+          <li class="w-4/12 flex flex-col justify-between lg:w-2/12">
             24/h price change %
-            <div class="flex flex-col items-center">
+            <div class="">
               <font-awesome-icon
                 class="text-3xl"
                 :icon="['fas', 'sort-up']"
@@ -54,7 +74,7 @@
           </li>
           <li v-if="!mobileView" class="lg:w-2/12">
             24/h volume
-            <div class="flex flex-col items-center">
+            <div class="flex flex-col ">
               <font-awesome-icon
                 class="text-3xl"
                 :icon="['fas', 'sort-up']"
@@ -64,7 +84,7 @@
           </li>
           <li v-if="!mobileView" class="lg:w-2/12">
             24/h volume change
-            <div class="flex flex-col items-center">
+            <div class="flex flex-col ">
               <font-awesome-icon
                 class="text-3xl"
                 :icon="['fas', 'sort-up']"
@@ -74,7 +94,7 @@
           </li>
           <li v-if="!mobileView" class="lg:w-2/12">
             24/h volume change %
-            <div class="flex flex-col items-center">
+            <div class="flex flex-col ">
               <font-awesome-icon
                 class="text-3xl"
                 :icon="['fas', 'sort-up']"
@@ -83,11 +103,20 @@
             </div>
           </li>
         </ul>
-        <CoinExchange
-          v-for="coin in sortedCoinData"
-          :key="coin.id"
-          :coin="coin"
-        />
+        <div v-if="!searchTerm">
+          <CoinExchange
+            v-for="coin in sortedCurrencyData(currencyData)"
+            :key="coin.id"
+            :coin="coin"
+          />
+        </div>
+        <div v-else>
+          <CoinExchange
+            v-for="coin in filteredCurrencyData"
+            :key="coin.id"
+            :coin="coin"
+          />
+        </div>
       </div>
     </section>
   </div>
@@ -98,14 +127,16 @@ export default {
   data() {
     return {
       mobileView: true,
-      coinData: [],
+      currencyData: [],
       orderBy: "",
-      orderOption: ""
+      orderOption: "",
+      isSearchOpen: false,
+      searchTerm: null
     };
   },
 
   async fetch() {
-    this.coinData = await fetch(
+    this.currencyData = await fetch(
       "https://api.nomics.com/v1/currencies/ticker?key=4c494109a687bef4f73e1e017317e61b&per-page=50&page=1"
     ).then(res => res.json());
   },
@@ -126,24 +157,31 @@ export default {
         this.orderOption = option;
         this.orderBy = by;
       }
-    }
-  },
+    },
 
-  computed: {
-    sortedCoinData: function() {
+    sortedCurrencyData(arr) {
       if (this.orderBy !== "price" && this.orderOption === "asc") {
-        return this.coinData.slice().sort((a, b) => {
+        return arr.slice().sort((a, b) => {
           return b["1d"][this.orderBy] - a["1d"][this.orderBy];
         });
       }
-
       if (this.orderOption === "asc") {
-        return this.coinData.slice().sort((a, b) => {
+        return arr.slice().sort((a, b) => {
           return b[this.orderBy] - a[this.orderBy];
         });
       }
 
-      return this.coinData;
+      return arr;
+    }
+  },
+
+  computed: {
+    filteredCurrencyData() {
+      const foundCurrency = this.currencyData.filter(coin =>
+        coin.id.includes(this.searchTerm.toUpperCase())
+      );
+
+      return this.sortedCurrencyData(foundCurrency);
     }
   },
 
