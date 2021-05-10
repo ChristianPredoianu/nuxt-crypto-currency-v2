cac74f22da193cd71b70e64c3e99eacc60bac608
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-05-10 13:03:18 +0200

    added favicon and changed meta title

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,14 +14,14 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: "CryptoInfo - A website for cryptocurrencies",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || ""
+        content: "CryptoInfo - A website for cryptocurrencies"
       }
     ],
     link: [
