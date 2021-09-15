7aef663d2ee9f81a204549c0f3b42a611fe07c4f
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-09-15 12:45:09 +0200

    made card a component

---
+++ components/AboutUsCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="flex flex-col w-3/4 px-8 py-16 mt-8 border border-gray-700 md:w-6/12 lg:w-3/12 shadow-xl "
+  >
+    <h2
+      class="inline-block text-xl pt-4 pb-2 border-b-2 align-text-top md:text-2xl"
+    >
+      {{ headerText }}
+    </h2>
+
+    <p class="mt-4 md:text-lg">
+      {{ cardText }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    headerText: {
+      type: String,
+      required: true
+    },
+    cardText: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
