f83049a553b78c0856828bc02a1f06b7cca9f60c
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-09-28 14:59:46 +0200

    added component for social icons

---
+++ components/SocialIcons.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <a :href="link.url" target="_blank">
+      <font-awesome-icon
+        class="text-3xl transition duration-200 transform hover:text-blue-800 hover:scale-110"
+        :icon="[
+          'fab',
+          link.type === 'twitter' ? `${link.type}-square` : link.type
+        ]"
+    /></a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
